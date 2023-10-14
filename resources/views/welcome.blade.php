301b20aeae99e1f1fecf5189a65ca3279b858ebe
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-10-13 21:39:09 -0300

    project init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <title>Coffee Delivery | Laravel</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="font-roboto antialiased bg-base-background">
+        <h1>Coffee Delivery | Laravel</h1>
+        <h1 class="font-baloo text-base-title">Coffee Delivery | Laravel</h1>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+            'roboto': ['Roboto', 'sans-serif'],
+            'baloo': ['"Baloo 2"', 'sans-serif']
+          },
+        colors: {
+            'product': {
+                'yellow-light': '#F1E9C9',
+                'yellow': '#DBAC2C',
+                'yellow-dark': '#C47F17',
+                'purple-light': '#EBE5F9',
+                'purple': '#8047F8',
+                'purple-dark': '#4B2995'
+            },
+            'base': {
+                'white': '#FFFFFF',
+                'background': '#FAFAFA',
+                'card': '#F3F2F2',
+                'input': '#EDEDED',
+                'button': '#E6E5E5',
+                'hover': '#D7D5D5',
+                'lable': '#8D8686',
+                'text': '#574F4D',
+                'subtitle': '#403937',
+                'title': '#272221'
+            }
+        }
+      },
+    },
+    plugins: [],
+  }
