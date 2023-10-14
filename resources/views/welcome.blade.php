79ba179efcd7320291d73653cbaba9b45431b598
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-10-14 14:48:44 -0300

    implements coffee cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,12 +73,144 @@
                     <li class="uppercase border border-product-yellow text-product-yellow-dark py-[6px] px-3 text-[10px] font-bold rounded-[100px] leading-[13px]">gelado</li>
                 </ul>
             </div>
-            <div>
-                <div class="bg-base-card w-64 h-[310px]">
-
+            <div class="grid grid-cols-4 gap-10 mt-8 mb-32">
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="bg-base-card w-64 h-[310px]">
-
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 px-5 items-center bg-base-card w-64 h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] shadow-inner">
+                    <img class="-mt-5" src="imgs/coffees/tradicional.png" alt="Tradicional">
+                    <div class="flex justify-center items-center gap-1">
+                        <span class="uppercase bg-product-yellow-light py-1 px-2 rounded-full text-[10px] text-product-yellow-dark font-bold">tradicional</span>
+                    </div>
+                    <h3 class="font-baloo text-xl mt-1 text-base-subtitle">Expresso Tradicional</h3>
+                    <p class="text-base-lable text-sm text-center">O tradicional café feito com água quente e grãos moídos</p>
+                    <div class="flex justify-between items-center w-[208px] mt-6">
+                        <span class="text-sm text-base-text">R$
+                            <strong class="font-baloo text-base-text text-2xl font-extrabold">9,90</strong>
+                        </span>
+                        <div class="flex justify-center items-center gap-2">
+                            <div class="bg-base-button p-2 rounded-md flex justify-center items-center gap-2">
+                                <i class="ph-bold ph-minus text-sm text-product-purple"></i>
+                                <span class="text-base-title">1</span>
+                                <i class="ph-bold ph-plus text-sm text-product-purple"></i>
+                            </div>
+                            <div class="flex justify-center items-center text-base-card p-2 rounded-md bg-product-purple-dark w-[38px] h-[38px]">
+                                <i class="text-icon ph-fill ph-shopping-cart"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
